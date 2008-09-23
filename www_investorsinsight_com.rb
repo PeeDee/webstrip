21fe8e073ea307dfb7986fd5589249b3484b264a
PeeDee <net.prophet.nz@gmail.com>
2008-09-23 14:08:41 +1200

    Mauldin outside the box added.

---
+++ www_investorsinsight_com.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+# This script strips a Mauldin MOTB page
+
+module Webstrip::Views
+
+  # @uri = 'http://www.investorsinsight.com/blogs/john_mauldins_outside_the_box/archive/2008/09/22/observations-on-a-crisis.aspx'
+  def www_investorsinsight_com
+  	hpr = Hpricot(open(@uri))
+		page = (hpr.at("div#CommonContent")).at("div.CommonContentBoxContent")
+    html do
+      head { 
+        title "Mauldin Outside the Box" 
+        style { '
+          .ContentBody { margin: 20px 20px 20px 20px;	}
+          .ContentBody img { margin: 20px 20px; } '
+        }
+      }
+      body { 
+        page.to_html
+      }
+    end
+  end
+  
+end
+
