1c74ce99fc4e4caf55a21c0e55814af16d543628
PeeDee <net.prophet.nz@gmail.com>
2008-11-14 12:18:12 +1300

    FT blog page added.

---
+++ blogs_ft_com.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+
+module Webstrip::Views
+
+  # ft blog, eg. http://blogs.ft.com/maverecon/2008/11/how-likely-is-a-sterling-crisis-or-is-london-really-reykjavik-on-thames/
+  def blogs_ft_com
+  	hpr = Hpricot(open(@uri))
+		page = (hpr.at("div#content")).at("div.post")
+    html do
+      head { 
+        title "FT Blog Page" 
+        style {  }
+      }
+      body { 
+        "<p>wbstrp'd from: <code><a href='#{uri}'>#{uri}</a></code></p><hr>" +
+        page.to_html
+      }
+    end
+  end
+  
+end
+
